48169cbfebc721d81caddee5019ec8eef6806bc5
Amir Gheybi <amir.962@yahoo.com>
2024-07-30 17:56:28 +0330

    hero with title

--- src/app/[locale]/Shop/page.tsx
+++ src/app/[locale]/Shop/page.tsx
@@ -3,16 +3,19 @@ import Category from "@/src/components/Shop/ShowCategory"
 import mainImg from "@/public/images/Shop/hero.jpg"
 import bgImage from "@/public/images/Shop/heroBg.jpg"
 import { Hero } from '@/ui/Hero'
+import { getTranslations } from 'next-intl/server'
 
 const Shop = async () => {
+  const t=await getTranslations("ShopPage.Hero")
+
   return (
     <main className='space-y-[5vmax] mb-7'>
       <Hero
         img={mainImg}
         bgImage={bgImage}
-        title={"title1"}
-        title2={"title2"}
-        desc={"title3"} />
+        title={t("title1")}
+        title2={t("title2")}
+        desc={t("title3")} />
       <Category />
       <Collection />
     </main>
@@ -24,12 +27,10 @@ export default Shop
 {
   /*
 
-import { getTranslations } from 'next-intl/server'
 import ProductPreview from '@/src/components/Shop/ShopItem'
 import Category from '@/src/components/Shop/ShowCategory'
 import getShopItems from '@/src/lib/getShopItems'
 
-  const t=await getTranslations("ShopPage.Hero")
   const products = await getShopItems()
 
 
