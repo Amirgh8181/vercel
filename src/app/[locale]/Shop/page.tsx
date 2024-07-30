c29781a42e6dfc8e4c79dc421c8372698476dfc2
Amir Gheybi <amir.962@yahoo.com>
2024-07-30 17:53:42 +0330

    add hero

--- src/app/[locale]/Shop/page.tsx
+++ src/app/[locale]/Shop/page.tsx
@@ -1,17 +1,19 @@
 import Collection from "@/src/components/Shop/othersection"
-import ProductPreview from "@/src/components/Shop/ShopItem"
 import Category from "@/src/components/Shop/ShowCategory"
-import getShopItems from "@/src/lib/getShopItems"
-
+import mainImg from "@/public/images/Shop/hero.jpg"
+import bgImage from "@/public/images/Shop/heroBg.jpg"
+import { Hero } from '@/ui/Hero'
 
 const Shop = async () => {
-  const products = await getShopItems()
-
   return (
     <main className='space-y-[5vmax] mb-7'>
-
+      <Hero
+        img={mainImg}
+        bgImage={bgImage}
+        title={"title1"}
+        title2={"title2"}
+        desc={"title3"} />
       <Category />
-      <ProductPreview products={products} />
       <Collection />
     </main>
   )
@@ -21,23 +23,18 @@ export default Shop
 
 {
   /*
-  import { Hero } from '@/ui/Hero'
-import mainImg from "@/public/images/Shop/hero.jpg"
-import bgImage from "@/public/images/Shop/heroBg.jpg"
+
 import { getTranslations } from 'next-intl/server'
 import ProductPreview from '@/src/components/Shop/ShopItem'
 import Category from '@/src/components/Shop/ShowCategory'
 import getShopItems from '@/src/lib/getShopItems'
 
   const t=await getTranslations("ShopPage.Hero")
+  const products = await getShopItems()
 
 
-        <Hero
-        img={mainImg}
-        bgImage={bgImage}
-        title={t("title1")}
-        title2={t("title2")}
-        desc={t("title3")} />
+
         
+      <ProductPreview products={products} />
   */
 }
