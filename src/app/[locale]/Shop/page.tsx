e5d7f4485ad2f5eacf3122dc15b8e94fc0194186
Amir Gheybi <amir.962@yahoo.com>
2024-07-30 17:34:59 +0330

    debug shop page

--- src/app/[locale]/Shop/page.tsx
+++ src/app/[locale]/Shop/page.tsx
@@ -1,15 +1,13 @@
-import ProductPreview from '@/src/components/Shop/ShopItem'
-import Category from '@/src/components/Shop/ShowCategory'
+
 import { Hero } from '@/ui/Hero'
 import mainImg from "@/public/images/Shop/hero.jpg"
 import bgImage from "@/public/images/Shop/heroBg.jpg"
-import Collection from '@/src/components/Shop/othersection'
 import { getTranslations } from 'next-intl/server'
 import getShopItems from '@/src/lib/getShopItems'
 
 const Shop = async () => {
   const products = await getShopItems()
-  const t=await getTranslations("ShopPage.Hero")
+  const t = await getTranslations("ShopPage.Hero")
   return (
     <main className='space-y-[5vmax] mb-7'>
       <Hero
@@ -18,11 +16,21 @@ const Shop = async () => {
         title={t("title1")}
         title2={t("title2")}
         desc={t("title3")} />
-      <Category />
-      <ProductPreview products={products} />
-      <Collection />
+
     </main>
   )
 }
 
 export default Shop
+
+{
+  /*
+import ProductPreview from '@/src/components/Shop/ShopItem'
+import Category from '@/src/components/Shop/ShowCategory'
+import Collection from '@/src/components/Shop/othersection'
+
+        <Category />
+      <ProductPreview products={products} />
+      <Collection />
+  */
+}
