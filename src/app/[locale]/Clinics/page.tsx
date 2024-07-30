0860981bfd7014dbabab2f1740a756759e593f95
Amir Gheybi <amir.962@yahoo.com>
2024-07-30 18:04:46 +0330

    add shop and clinic page req to debug

--- src/app/[locale]/Clinics/page.tsx
+++ src/app/[locale]/Clinics/page.tsx
@@ -2,9 +2,12 @@ import { Hero } from '@/ui/Hero'
 import mainImg from "@/public/images/Clinics/hero-pic.jpg"
 import bgImage from "@/public/images/Clinics/hero bg.jpg"
 import { getTranslations } from 'next-intl/server';
+import getClinics from '@/src/lib/getClinics';
 
 const Clinics = async () => {
     const t = await getTranslations("Clinic")
+    const req = await getClinics()
+
     return (
         <>
             <Hero
@@ -22,10 +25,8 @@ export default Clinics
 
 {/*
 import ClinicAndShelterCard from '@/src/components/UI/clinicAndShelterCard'
-import getClinics from '@/src/lib/getClinics';
 
 
-        const req = await getClinics()
 
                 <div className='w-full min-h-screen mt-6'>
                 <ClinicAndShelterCard href='/Clinics/' data={req} headerText={t("Card.head")} />
--- src/app/[locale]/Shop/page.tsx
+++ src/app/[locale]/Shop/page.tsx
@@ -4,10 +4,11 @@ import mainImg from "@/public/images/Shop/hero.jpg"
 import bgImage from "@/public/images/Shop/heroBg.jpg"
 import { Hero } from '@/ui/Hero'
 import { getTranslations } from 'next-intl/server'
+import getShopItems from '@/src/lib/getShopItems'
 
 const Shop = async () => {
   const t=await getTranslations("ShopPage.Hero")
-
+  const products = await getShopItems()
   return (
     <main className='space-y-[5vmax] mb-7'>
       <Hero
@@ -28,9 +29,8 @@ export default Shop
   /*
 
 import ProductPreview from '@/src/components/Shop/ShopItem'
-import getShopItems from '@/src/lib/getShopItems'
 
-  const products = await getShopItems()
+  
 
 
 
