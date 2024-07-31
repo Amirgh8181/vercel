0a9617d513f62aea7b64a4fd4ed16e13180960d5
Amir Gheybi <amir.962@yahoo.com>
2024-07-31 15:28:15 +0330

    qqq

--- src/lib/getClinics/index.ts
+++ src/lib/getClinics/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const getClinics = async () => {
     const url = process.env.NEXT_PUBLIC_CLIENT_URL as string
-    const req = await axios.get(`${url}/api/clinics`)
+    const req = await axios.get(`${url}/api/auth/clinics`)
     console.log(req.data);
     
     if (!req.data) {
